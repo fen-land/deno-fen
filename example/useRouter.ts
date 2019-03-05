5a14ed0c5dc8b44dce6f3fddb52dd0919edc19b4
mxz96102 <mxz96102@qq.com>
2019-03-05 14:38:03 +0800

    feat: router is available

---
+++ example/useRouter.ts
@@ -0,0 +1,38 @@
+import { Server } from "../src/server.ts";
+import { Router } from "../src/tool/router.ts";
+
+const s = new Server();
+
+s.port = 1882;
+
+s.logger.changeLevel('ALL');
+
+let mergeRouter = new Router('merge');
+
+mergeRouter
+  .get('', async (ctx) => ctx.body = `${ctx.router.name} in ${ctx.router.route}`)
+  .post('', async (ctx) => ctx.body = `POST ${ctx.router.name} in ${ctx.router.route}`)
+  .get('me', async (ctx) => ctx.body = `${ctx.router.name} in ${ctx.router.route}`);
+
+let router = new Router();
+
+router
+  .get('/:id', async (ctx) => {
+  ctx.body = `we have ${JSON.stringify(ctx.router.params)} in ${ctx.router.route}`
+})
+  .get('/:id/:name', async (ctx) => {
+  ctx.body = `we have ${JSON.stringify(ctx.router.params)} in ${ctx.router.route}`
+})
+  .get('/hello/:name', async (ctx) => {
+    ctx.body = `hello ${ctx.router.params.name} in ${ctx.router.route}`
+  })
+  .use({
+    '/use': {get: async (ctx) => ctx.body = `use in ${ctx.router.route}`}
+  })
+  .merge('/merge', mergeRouter);
+;
+
+// it will respond file from the path where deno run
+s.setController(router.controller);
+
+s.start();
